10ffbef4108521428e8651ffcf9997452da33417
so2emma <osoemmanuel1969@gmail.com>
2023-11-06 09:28:01 +0100

    Dynamic value and listing in react.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App(){
-    return <h1>Hello from the app Component</h1>
+function App() {
+
+    const title = "Blog Post"
+    const body = "this is my blog post"
+    const comments = [
+        {id:1, text: 'Comment one'},
+        {id:2, text: 'Comment two'},
+        {id:3, text: 'Comment three'},
+    ]
+
+  return <div className="conatainer">
+  <h1>{title.toUpperCase()}</h1>
+  <p>{body}</p>
+  <div className="comments">
+    <h3>Comments ({comments.length})</h3>
+    <ul>
+        {comments.map((comment,index) => (
+            <li key={index}>{comment.text}</li>
+        ))}
+    </ul>
+  </div>
+  </div>;
 }
 
-export default App
+export default App;
