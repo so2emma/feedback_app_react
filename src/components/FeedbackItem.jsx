849125d2f5d6a7fe95fcefa3502f330d176e683a
so2emma <osoemmanuel1969@gmail.com>
2023-11-07 11:29:45 +0100

    card component and conditional

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react"
+import Card from "./Card"
+import PropTypes from 'prop-types'
 
-function FeedbackItem() {
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState("This is am example of a Feedback.")
 
-    const handleClick = () => {
-        setRating((prev) => {
-            return prev + 1
-        })
-    }
-    
+function FeedbackItem({item}) {
+
   return (
-    <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display"> {text} </div>
-        <button onClick={handleClick}>Click</button>
-    </div>
+    <Card >
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display"> {item.text} </div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
