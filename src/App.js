4d7c3a8bc6c51bc01c7dfc373ad62cf042bc1ffd
so2emma <osoemmanuel1969@gmail.com>
2023-11-07 14:03:00 +0100

    button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
     <>
       <Header />
       <div className="conatainer">
+        <FeedbackForm />
         <FeedbackStats feedback = {feedback} />
         <FeedbackList feedback={feedback} 
         handleDelete={deleteFeedback}/>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,31 @@
+import Card from "./shared/Card";
+import { useState } from "react";
+import Button from "./shared/Button";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* { @todo - rating select component} */}
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="write a review"
+            value={text}
+          />
+          <Button type="submit">Send</Button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
